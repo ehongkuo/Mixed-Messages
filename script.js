b6206b11b7f7871b0bd4f33e970702a2bf3488f0
Ehong Kuo <ehongkuo@gmail.com>
2021-07-29 17:08:29 -0700

    working main

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const characters = {
-    movies: [],
-    shows: [],
-    anime: []
+    movies: ['Iron Man', 'Superman', 'Black Widow', 'Batman', 'Darth Vader', 'Gandalf', 'James Bond', 'Harry Potter', 'Voldemort', 'Captain America', 'Indiana Jones', 'Optimus Prime', 'Yoda', 'Wolverine', 'Spider Man'],
+    shows: ['Joey Tribbiani', 'Chandler Bing', 'Rachel Green', 'Jake Peralta', 'Captain Holt', 'Michael Scott', 'Yoo Jaesuk', 'Lee Kwangsoo', 'Kim Jungkook', 'Kang Hodong', 'Kim Heechul', 'Lee Soogeun'],
+    anime: ['Eren Yeager', 'Mikasa Ackermann', 'Levi Ackermann', 'Light Yagami', 'Tanjiro Kamado', 'Saitama', 'Norman', 'Edward Elric', 'Naruto Uzumaki', 'Sasuke Uchiha', 'Itachi Uchiha', 'Rengoku Kyojuro']
+}
+
+const randomGenerator = num => {
+    return Math.floor(Math.random() * num);
+}
+
+let message = [];
+for (let prop in characters) {
+    
 }
